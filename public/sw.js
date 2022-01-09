e1d859a1c3c4afc4f16746e6eb21a95bd3488198
Ezequiel <ezequielamin@outlook.com>
2022-01-09 03:24:50 -0300

    intento de pwa

--- public/sw.js
+++ public/sw.js
@@ -44,7 +44,7 @@ var returnFromCache = function (request) {
   return caches.open("offline").then(function (cache) {
     return cache.match(request).then(function (matching) {
       if (!matching || matching.status == 404) {
-        return cache.match("offline.html");
+        return cache.match("/pages/offline.html");
       } else {
         return matching;
       }
