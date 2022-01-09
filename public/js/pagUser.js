779e678ed6fd51a533dd3b7157cd49b0480a19bf
Ezequiel <ezequielamin@outlook.com>
2022-01-09 02:55:33 -0300

    agrego deteccion automatica de periodo

--- public/js/pagUser.js
+++ public/js/pagUser.js
@@ -5,7 +5,14 @@ import {
   validarCamposNuevoIngreso,
 } from "./validaciones.js";
 import { Gasto } from "./gasto.js";
-import { loadInfo, actualizarInfo, cargarGasto, loadGastos, loadDeudas, transferOldData } from "./DB.js";
+import {
+  loadInfo,
+  actualizarInfo,
+  cargarGasto,
+  loadGastos,
+  loadDeudas,
+  transferOldData,
+} from "./DB.js";
 
 let campoImporte = document.getElementById("importe");
 let campoCuenta = document.getElementById("cuenta");
@@ -20,16 +27,24 @@ let botonTrasferirDatos = document.getElementById("btnTransferirDatos");
 let botonModificarLimite = document.getElementById("btnModificarLimite");
 let progressBar = document.getElementById("progressBar");
 
-let transferencia=false;
-let total, totalPropio, totalPapas, diferencia,efectivoPropio,efectivoPapas,TC,TCPropio,TD;
+let transferencia = false;
+let total,
+  totalPropio,
+  totalPapas,
+  diferencia,
+  efectivoPropio,
+  efectivoPapas,
+  TC,
+  TCPropio,
+  TD;
 let info = await loadInfo(false);
 cargarGastos().then(() => {
   progressBar.style.width = "0%";
   document.getElementById("loadingSpinner").style.opacity = "0";
-  setTimeout(()=>{
-    document.getElementById("loadingSpinner").style.display = "none"
+  setTimeout(() => {
+    document.getElementById("loadingSpinner").style.display = "none";
     loadProgressBar();
-  },350);
+  }, 350);
 });
 
 campoImporte.addEventListener("blur", () => {
@@ -48,7 +63,7 @@ botonBorrar.addEventListener("click", () => {
   iniciarNuevoPeriodo();
 });
 botonTransferencia.addEventListener("click", () => {
-  transferencia=true;
+  transferencia = true;
 
   document.getElementById("tituloModal").innerHTML = "Transferir fondos";
   document.getElementById("lblCuenta").innerHTML = "Cuenta Destino";
@@ -57,7 +72,7 @@ botonTransferencia.addEventListener("click", () => {
   document.getElementById("btnAñadir").classList = "btn btn-warning mt-2";
 });
 botonAñadir.addEventListener("click", () => {
-  transferencia=false;
+  transferencia = false;
 
   document.getElementById("tituloModal").innerHTML = "Añadir fondos";
   document.getElementById("lblCuenta").innerHTML = "Cuenta";
@@ -65,9 +80,7 @@ botonAñadir.addEventListener("click", () => {
   document.getElementById("btnAñadir").innerHTML = "Añadir";
   document.getElementById("btnAñadir").classList = "btn btn-success mt-2";
 });
-botonModificarLimite.addEventListener("click", () => {
-
-});
+botonModificarLimite.addEventListener("click", () => {});
 /*botonTrasferirDatos.addEventListener("click", () => {
   transferirDatos();
 });*/
@@ -79,7 +92,7 @@ function guardarFondeo(e) {
   e.preventDefault();
 
   if (validarCamposNuevoIngreso()) {
-    if(!transferencia) cargarFondo();
+    if (!transferencia) cargarFondo();
     else cargarTransferencia();
   }
 }
@@ -144,20 +157,26 @@ async function cargarGastos() {
   let aFavor = 0;
 
   gastos.forEach((gasto) => {
-    if (gasto.pago == "Efectivo" && gasto.origen == "Plata propia" && gasto.categoria != "Fondeo")
+    if (
+      gasto.pago == "Efectivo" &&
+      gasto.origen == "Plata propia" &&
+      gasto.categoria != "Fondeo"
+    )
       efectivoPropio += parseFloat(gasto.importe);
     else if (gasto.pago == "Efectivo" && gasto.origen == "Plata papas")
       efectivoPapas += parseFloat(gasto.importe);
-    else if (gasto.pago == "TC" && gasto.origen=="Plata propia") TCPropio += parseFloat(gasto.importe);
-    else if (gasto.pago == "TD" && gasto.categoria != "Fondeo") TD += parseFloat(gasto.importe);
+    else if (gasto.pago == "TC" && gasto.origen == "Plata propia")
+      TCPropio += parseFloat(gasto.importe);
+    else if (gasto.pago == "TD" && gasto.categoria != "Fondeo")
+      TD += parseFloat(gasto.importe);
 
-    if(gasto.debo) debo+=parseFloat(gasto.importe);
+    if (gasto.debo) debo += parseFloat(gasto.importe);
   });
 
   const deudas = await loadDeudas(true);
 
   deudas.forEach((deuda) => {
-    for(let i=0;i<deuda.lista.length;i++){
+    for (let i = 0; i < deuda.lista.length; i++) {
       aFavor += parseFloat(deuda.lista[i].importe);
     }
   });
@@ -175,14 +194,16 @@ async function cargarGastos() {
 
   let totalPropioPorcentaje;
   let totalPapasPorcentaje;
-  if(total!=0){
+  if (total != 0) {
     totalPropioPorcentaje = Math.round((totalPropio * 100) / total);
     totalPapasPorcentaje = Math.round((totalPapas * 100) / total);
   }
 
   document.getElementById("totalGastos").innerHTML = "$" + total;
   document.getElementById("totalGastos2").innerHTML = "$" + total;
-  if(total!=0) document.getElementById("porcentajes").innerHTML = totalPropioPorcentaje + "% / " + totalPapasPorcentaje + "%";
+  if (total != 0)
+    document.getElementById("porcentajes").innerHTML =
+      totalPropioPorcentaje + "% / " + totalPapasPorcentaje + "%";
   else document.getElementById("porcentajes").innerHTML = "0% / 0%";
   document.getElementById("totalPropio").innerHTML = "$" + totalPropio;
 
@@ -192,11 +213,28 @@ async function cargarGastos() {
   let fechaInicial = info.fecha.split("/");
   let fechaFinal = new Date();
   let fechaFinalPeriodo = getFechaFinalPeriodo(fechaInicial);
-  diferencia = Math.floor((fechaFinal.getTime() - new Date(fechaInicial[2], fechaInicial[1]-1, fechaInicial[0]).getTime()) / (1000 * 3600 * 24)) + 1 ;
-  let diasRestantes = Math.floor((fechaFinalPeriodo.getTime() - fechaFinal.getTime()) / (1000 * 3600 * 24)) + 1 ;
+  diferencia =
+    Math.floor(
+      (fechaFinal.getTime() -
+        new Date(
+          fechaInicial[2],
+          fechaInicial[1] - 1,
+          fechaInicial[0]
+        ).getTime()) /
+        (1000 * 3600 * 24)
+    ) + 1;
+  let diasRestantes =
+    Math.floor(
+      (fechaFinalPeriodo.getTime() - fechaFinal.getTime()) / (1000 * 3600 * 24)
+    ) + 1;
 
-  if(diasRestantes!=1) document.getElementById("diasRestantes").innerHTML = diasRestantes + " días";
-  else document.getElementById("diasRestantes").innerHTML = diasRestantes + " día";
+  if (diasRestantes != 1)
+    document.getElementById("diasRestantes").innerHTML =
+      "Quedan " + diasRestantes + " días";
+  else
+    document.getElementById("diasRestantes").innerHTML = "Queda " + diasRestantes + " día";
+
+  if (diasRestantes <= 3) document.getElementById("diasRestantesCont").className = "mb-0 text-danger fw-bold"
 
   fechaFinal =
     fechaFinal.getDate() +
@@ -217,51 +255,73 @@ async function cargarGastos() {
   document.getElementById("limiteEstado").innerHTML = "$" + info.limite;
   document.getElementById("limiteEstadoBar").innerHTML = "$" + info.limite;
 
+  if (diasRestantes <= 0) {
+    Swal.fire({
+      title: "¡Periodo finalizado!",
+      text: "El periodo de este mes ya ha finalizado. Se iniciará uno nuevo.",
+      timer: 2500,
+      showCancelButton: false,
+      showConfirmButton: false,
+    }).then(async () => {
+      await iniciarNuevoPeriodo();
+    });
+  }
+
   return total;
 }
 
-function loadProgressBar(){
+function loadProgressBar() {
   let limite = info.limite;
 
-  let progreso = Math.round((total/limite)*100);
+  let progreso = Math.round((total / limite) * 100);
   progressBar.style.width = progreso + "%";
   progressBar.innerHTML = progreso + "%";
 
-  if(progreso>=85){
-    progressBar.className = "progress-bar bg-danger progress-bar-striped progress-bar-animated";
-  }
-  else if(progreso>=75){
-    progressBar.className = "progress-bar bg-warning progress-bar-striped progress-bar-animated";
+  if (progreso >= 85) {
+    progressBar.className =
+      "progress-bar bg-danger progress-bar-striped progress-bar-animated";
+  } else if (progreso >= 75) {
+    progressBar.className =
+      "progress-bar bg-warning progress-bar-striped progress-bar-animated";
   }
 
   let restante = limite - total;
   document.getElementById("saldoRestante").innerHTML = "$" + restante;
 }
 
-function getFechaFinalPeriodo(fechaInicial){
+function getFechaFinalPeriodo(fechaInicial) {
   let fechaFinal;
-  if(fechaInicial[0]==31){
-    if(fechaInicial[1]!=7 && fechaInicial[1]!=12) fechaInicial[0]=30;
+  if (fechaInicial[0] == 31) {
+    if (fechaInicial[1] != 7 && fechaInicial[1] != 12) fechaInicial[0] = 30;
   }
-  if(fechaInicial[0]>28 && fechaInicial[1]==1){
-    fechaInicial[0]-=28;
-    fechaInicial[1]=3;
+  if (fechaInicial[0] > 28 && fechaInicial[1] == 1) {
+    fechaInicial[0] -= 28;
+    fechaInicial[1] = 3;
   }
 
-  if(fechaInicial[1]!=12) fechaFinal = new Date(fechaInicial[2], fechaInicial[1]-1, parseInt(fechaInicial[0])-1);
-  else fechaFinal = new Date(parseInt(fechaInicial[2])+1, 0, parseInt(fechaInicial[0])-1);
+  if (fechaInicial[1] != 12)
+    fechaFinal = new Date(
+      fechaInicial[2],
+      fechaInicial[1] - 1,
+      parseInt(fechaInicial[0]) - 1
+    );
+  else
+    fechaFinal = new Date(
+      parseInt(fechaInicial[2]) + 1,
+      0,
+      parseInt(fechaInicial[0]) - 1
+    );
 
   return fechaFinal;
 }
 
-async function cargarTransferencia(){
+async function cargarTransferencia() {
   switch (campoCuenta.value) {
     case "Efectivo": {
-      if(info.saldoTD >= parseFloat(campoImporte.value)){
+      if (info.saldoTD >= parseFloat(campoImporte.value)) {
         info.saldoEfectivo += parseFloat(campoImporte.value);
         info.saldoTD -= parseFloat(campoImporte.value);
-      }
-      else{
+      } else {
         Swal.fire({
           title: "Error",
           text: "No tienes saldo suficiente",
@@ -273,11 +333,10 @@ async function cargarTransferencia(){
       break;
     }
     default: {
-      if(info.saldoEfectivo >= parseFloat(campoImporte.value)){
+      if (info.saldoEfectivo >= parseFloat(campoImporte.value)) {
         info.saldoEfectivo -= parseFloat(campoImporte.value);
         info.saldoTD += parseFloat(campoImporte.value);
-      }
-      else{
+      } else {
         Swal.fire({
           title: "Error",
           text: "No tienes saldo suficiente",
@@ -306,8 +365,18 @@ async function cargarTransferencia(){
   });
 }
 
-async function iniciarNuevoPeriodo(){
-  await transferOldData(total,totalPropio,totalPapas,diferencia,efectivoPropio,efectivoPapas,TC,TCPropio,TD);
+async function iniciarNuevoPeriodo() {
+  await transferOldData(
+    total,
+    totalPropio,
+    totalPapas,
+    diferencia,
+    efectivoPropio,
+    efectivoPapas,
+    TC,
+    TCPropio,
+    TD
+  );
   Swal.fire({
     title: "Nuevo periodo",
     text: "Felicidades, sobreviviste un nuevo mes",
@@ -319,7 +388,7 @@ async function iniciarNuevoPeriodo(){
   });
 }
 
-function guardarLimite(e){
+function guardarLimite(e) {
   e.preventDefault();
   info.limite = parseFloat(campoLimite.value);
   actualizarInfo(info);
@@ -346,12 +415,17 @@ const transferirDatos = async () => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({info: info, gastos: gastos, deudas: deudas, proxPagos: proxPagos}),
-  })
+    body: JSON.stringify({
+      info: info,
+      gastos: gastos,
+      deudas: deudas,
+      proxPagos: proxPagos,
+    }),
+  });
   let body = await res.text();
   body = JSON.parse(body);
-  
-  if(body.code == "200"){
+
+  if (body.code == "200") {
     Swal.fire({
       title: "Transferencia realizada",
       text: "Se han transferido correctamente los datos",
@@ -363,4 +437,4 @@ const transferirDatos = async () => {
       window.location.href = "/pages/user.html";
     });
   }
-}
+};
